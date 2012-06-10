b3d46e4f6e6dbaafcfd1cac032321d7668fdb8dc
Diego Rey Mendez <diego.reymendez@globant.com>
2012-06-10 02:44:52 -0300

    Initial Commit

---
+++ Horizontal TableView Example/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Horizontal TableView Example
+//
+//  Created by Diego Rey Mendez on 6/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
---
+++ Horizontal TableView ExampleTests/Horizontal_TableView_ExampleTests.h
@@ -0,0 +1,13 @@
+//
+//  Horizontal_TableView_ExampleTests.h
+//  Horizontal TableView ExampleTests
+//
+//  Created by Diego Rey Mendez on 6/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Horizontal_TableView_ExampleTests : SenTestCase
+
+@end
